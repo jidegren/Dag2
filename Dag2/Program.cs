66e7cf59ae94189a8a5de47368fc15442a688b4a
Robin Jidegren <robin_jide@hotmail.com>
2021-01-19 10:20:33 +0100

    Initial commit

---
+++ Dag2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Dag2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int heltal = 10;
+            Console.WriteLine(heltal);
+        }
+    }
+}
